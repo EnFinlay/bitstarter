87643cab74f32a02a58a3e6de32334d2da4c0d13
Eric Finlay <enfinlay@gmail.com>
2013-07-05 22:43:27 +0000

    Read web.js message from index.html
    Signed-off-by: Eric Finlay <enfinlay@gmail.com>

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileBuff = fs.readFileSync('index.html');
+  response.send(fileBuff.toString());
 });
 
 var port = process.env.PORT || 5000;
